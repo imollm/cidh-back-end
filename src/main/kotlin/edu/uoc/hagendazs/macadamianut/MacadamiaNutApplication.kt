9a1a79513d34947866fbe1c47035b993a1dd1052
Ivan Gonzalez <igonzalez@smiletronix.com>
2021-11-15 21:43:19 +0100

    CIDH-60
    Backend init

---
+++ src/main/kotlin/edu/uoc/hagendazs/macadamianut/MacadamiaNutApplication.kt
@@ -0,0 +1,11 @@
+package edu.uoc.hagendazs.macadamianut
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MacadamiaNutApplication
+
+fun main(args: Array<String>) {
+	runApplication<MacadamiaNutApplication>(*args)
+}
