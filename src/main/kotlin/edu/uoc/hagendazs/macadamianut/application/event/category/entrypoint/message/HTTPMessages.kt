12140478a42342e4d629ab8ab2e7f9536ea2ca38
Ivan Moll Moll <imollm@uoc.edu>
2021-12-08 13:27:43 +0100

    CIDH-109 HTTPMessages now can be used for every component

--- src/main/kotlin/edu/uoc/hagendazs/macadamianut/application/event/category/entrypoint/message/HTTPMessages.kt
+++ src/main/kotlin/edu/uoc/hagendazs/macadamianut/application/event/category/entrypoint/message/HTTPMessages.kt
@@ -1,7 +1,9 @@
 package edu.uoc.hagendazs.macadamianut.application.event.category.entrypoint.message
 
 enum class HTTPMessages(val message: String) {
+    CREATED("The resource was created successfully"),
     MISSING_VALUES("Some fields doesn't have value"),
-    ALREADY_EXISTS("This category already exists"),
-    NOT_FOUND("This category not exists")
+    ALREADY_EXISTS("This resource already exists"),
+    NOT_FOUND("This resource not exists"),
+    ERROR_SAVING_A_RESOURCE("Error saving resource")
 }
