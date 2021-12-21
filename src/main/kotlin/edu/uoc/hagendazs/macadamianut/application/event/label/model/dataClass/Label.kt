dfb606b8ce7e6ae4c4e05b3b638bd03376698473
Ivan Moll <imollm@uoc.edu>
2021-12-21 18:24:13 +0000

    CIDH-37 Fix package

--- src/main/kotlin/edu/uoc/hagendazs/macadamianut/application/event/label/model/dataClass/Label.kt
+++ src/main/kotlin/edu/uoc/hagendazs/macadamianut/application/event/label/model/dataClass/Label.kt
@@ -1,4 +1,4 @@
-package edu.uoc.hagendazs.macadamianut.application.event.category.model.dataClass
+package edu.uoc.hagendazs.macadamianut.application.event.label.model.dataClass
 
 import java.time.LocalDateTime
 import java.util.UUID
